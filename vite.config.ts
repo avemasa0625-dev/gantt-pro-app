901a76975eac3f79c7878a971ad24708c1e2866b
Abe <>
2026-01-13 18:51:53 +0900

    住所設定（base path）の修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/gantt-pro-app/', // ここを追加しました！
+  // ↓ここを現在のリポジトリ名に合わせるのがポイントです
+  base: "/gantt-pro-app/", 
 })
